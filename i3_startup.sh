95112bf2ed273e2b02c4a74302c14bf2bb21efff
David Gomes <gomes@memsql.com>
2018-10-07 16:12:25 +0100

    improve locking, disable system bell, improve startup script

--- i3_startup.sh
+++ i3_startup.sh
@@ -1,19 +1,30 @@
-#set -x
+set -x
 
 #xrandr --output eDP-1 --mode 1920x1080
 setxkbmap -option -option caps:escape
 nm-applet &
-solaar &
+
+if ! pgrep -x "solaar" > /dev/null
+then
+	solaar &
+fi
+
 xcompmgr &
 nitrogen --restore &
 # id 15 is logitech mx master
 xinput --set-prop 15 "Device Accel Constant Deceleration" 15
-# id 11 is my touchpad, not my trackpad
-xinput --set-prop 16 "libinput Tapping Enabled" 1
-xinput --set-prop 16 "libinput Natural Scrolling Enabled" 1
+# id  is my touchpad, not my trackpad
+
+TOUCHPAD_ID=$(xinput --list | grep TouchPad | awk -F ' ' '{ print $6 }' | awk -F '=' '{ print $2 }')
+
+echo $TOUCHPAD_ID
+
+xinput --set-prop $TOUCHPAD_ID "libinput Tapping Enabled" 1
+xinput --set-prop $TOUCHPAD_ID "libinput Natural Scrolling Enabled" 1
 xset r rate 200 20
 
 xset s off
 xset -dpms
 xset s noblank
 
+xset -b
